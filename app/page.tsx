1affdf60300cc4efb60b6707f16c5bac979c452a
Muhammad Adil <anybhai6@gmail.com>
2026-01-10 01:11:22 +0500

    Refactor Cal embed script with new configuration
    Updated the Cal inline embed code with new parameters and link.

--- app/page.tsx
+++ app/page.tsx
@@ -511,7 +511,59 @@ export default function Home() {
               <div className="w-embed w-script">
                 {/* Cal inline embed code begins */}
                 <div style={{ width: '100%', height: '100%', overflow: 'scroll' }} id="my-cal-inline"></div>
-                <Script id="cal-com-embed" strategy="afterInteractive">
+
+                                    <Script id="cal-com-embed" strategy="afterInteractive">
+                        {`
+                    (function (C, A, L) {
+                        let p = function (a, ar) { a.q.push(ar); };
+                        let d = C.document;
+                        C.Cal = C.Cal || function () {
+                        let cal = C.Cal;
+                        let ar = arguments;
+                        if (!cal.loaded) {
+                            cal.ns = {};
+                            cal.q = cal.q || [];
+                            d.head.appendChild(d.createElement('script')).src = A;
+                            cal.loaded = true;
+                        }
+                        if (ar[0] === L) {
+                            const api = function () { p(api, arguments); };
+                            const namespace = ar[1];
+                            api.q = api.q || [];
+                            if (typeof namespace === 'string') {
+                            cal.ns[namespace] = cal.ns[namespace] || api;
+                            p(cal.ns[namespace], ar);
+                            p(cal, ['initNamespace', namespace]);
+                            } else p(cal, ar);
+                            return;
+                        }
+                        p(cal, ar);
+                        };
+                    })(window, 'https://app.cal.com/embed/embed.js', 'init');
+
+                    Cal('init', '30min', { origin: 'https://cal.com' });
+
+                    Cal.ns['30min']('inline', {
+                        elementOrSelector: '#my-cal-inline',
+                        
+                        // I UPDATED THIS LINK BASED ON YOUR NEW SCREENSHOT:
+                        calLink: 'clarity-call-with-adil/30min', 
+                        
+                        layout: 'month_view',
+                    });
+
+                    Cal.ns['30min']('ui', {
+                        theme: 'light',
+                        styles: { branding: { brandColor: '${color}' } },
+                        hideEventTypeDetails: false,
+                        layout: 'month_view',
+
+                        
+                    });
+                `}
+                    </Script>
+
+                {/* <Script id="cal-com-embed" strategy="afterInteractive">
                   {`
                     (function (C, A, L) {
                       let p = function (a, ar) {
@@ -558,7 +610,7 @@ export default function Home() {
                       layout: 'month_view',
                     });
                   `}
-                </Script>
+                </Script> */}
                 {/* Cal inline embed code ends */}
               </div>
             </div>
